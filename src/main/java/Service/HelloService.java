7d73fd29394f3cb7e127d5e01b17b568e948ace5
zhengbin_Mac <1746788394@qq.com>
2016-04-08 20:51:16 +0800

    Dubbo-provider

---
+++ src/main/java/Service/HelloService.java
@@ -0,0 +1,5 @@
+package Service;
+
+public interface HelloService {
+	public String sayHello();
+}
---
+++ src/main/java/ServiceImpl/HelloServiceImpl.java
@@ -0,0 +1,11 @@
+package ServiceImpl;
+
+import Service.HelloService;
+
+public class HelloServiceImpl implements HelloService{
+
+	public String sayHello() {
+		return "Hello World";
+	}
+
+}
